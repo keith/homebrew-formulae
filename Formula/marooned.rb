6660ed3bf434db41f065bb013557b6207a0b1d0f
Keith Smiley <keithbsmiley@gmail.com>
2014-08-18 09:59:38 -0700

    marooned: Release version 0.1.1

--- Formula/marooned.rb
+++ Formula/marooned.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Marooned < Formula
   homepage "https://github.com/Keithbsmiley/marooned/"
-  url "http://keithbsmiley.github.io/marooned/marooned-0.1.0.tar.gz"
-  sha1 "11d8fe56431dce5adad9dc251903bb6042578d0c"
+  url "http://keithbsmiley.github.io/marooned/marooned-0.1.1.tar.gz"
+  sha1 "f56bbbe5b4d4464fac71c297f184cd93b8324002"
 
   depends_on "xcproj" => :recommended
 
