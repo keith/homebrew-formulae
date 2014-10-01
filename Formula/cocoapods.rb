464bc086bfc38afb80e3e85a843757b3c7132d26
Keith Smiley <keithbsmiley@gmail.com>
2014-10-01 11:36:12 -0700

    cocoapods: Release version 0.34.1

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -3,7 +3,7 @@ require "formula"
 class Cocoapods < Formula
   homepage "https://github.com/Keithbsmiley/cocoapods/"
   url "http://keithbsmiley.github.io/CocoaPods/cocoapods-0.34.1.tar.gz"
-  sha1 "858d27629a8001c3b8128b85576d0ec9800fd485"
+  sha1 "4965b0d686989a3f8ce13093e3d876d99076d40c"
 
   depends_on "xcproj" => :recommended
 
@@ -11,7 +11,7 @@ class Cocoapods < Formula
     prefix.install "vendor"
     prefix.install "lib" => "rubylib"
 
-    bin.install "src/cocoapods"
+    bin.install "src/pod"
   end
 
   test do
