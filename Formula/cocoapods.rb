2ad5c24fd2eaf48905255bc4c7d1879ed6eb1741
Keith Smiley <keithbsmiley@gmail.com>
2015-05-27 16:23:02 -0700

    Add patch for CocoaPods plugins until next release

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -7,6 +7,8 @@ class Cocoapods < Formula
 
   depends_on "xcproj" => :recommended
 
+  patch :DATA
+
   def install
     prefix.install "vendor"
     prefix.install "lib" => "rubylib"
@@ -18,3 +20,17 @@ class Cocoapods < Formula
     system "#{bin}/cocoapods", "--version"
   end
 end
+
+__END__
+diff --git i/src/pod w/src/pod
+index 999a11d..8424809 100755
+--- i/src/pod
++++ w/src/pod
+@@ -9,7 +9,7 @@ require gems_setup
+
+ $LOAD_PATH.unshift(File.expand_path("../../rubylib", file_path))
+
+-Dir.glob(File.join(File.expand_path("../../"), "cocoapods-*/*/lib")) do |dir|
++Dir.glob(File.join(File.expand_path("../../../../", file_path), "cocoapods-*/*/lib")) do |dir|
+   $LOAD_PATH.unshift(dir)
+ end
