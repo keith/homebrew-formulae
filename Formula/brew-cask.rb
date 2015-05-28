35b89ee5353a802e616846c7a852c9d851edca9f
Keith Smiley <keithbsmiley@gmail.com>
2015-05-28 08:16:18 -0700

    Brew cask patch, uninstall after copy
    This changes the behavior of brew cask to after copying the new cask,
    also uninstall the files from /opt/homebrew-cask/Caskroom. This is just
    to cleanup after ourselves.

--- Formula/brew-cask.rb
+++ Formula/brew-cask.rb
@@ -21,28 +21,57 @@ end
 
 __END__
 diff --git i/lib/hbc/artifact/hardlinked.rb w/lib/hbc/artifact/hardlinked.rb
-index 8b96c55..e66d7f1 100644
+index 8b96c55..588b0a1 100644
 --- i/lib/hbc/artifact/hardlinked.rb
 +++ w/lib/hbc/artifact/hardlinked.rb
-@@ -10,7 +10,7 @@ class Hbc::Artifact::Hardlinked < Hbc::Artifact::Symlinked
+@@ -5,13 +5,13 @@ class Hbc::Artifact::Hardlinked < Hbc::Artifact::Symlinked
+   end
+
+   def self.link_type_english_name
+-    'Hardlink'
++    'Copy'
+   end
 
    def create_filesystem_link(source, target)
      Pathname.new(target).dirname.mkpath
 -    @command.run!('/bin/ln', :args => ['-hf', '--', source, target])
+-    add_altname_metadata source, target
 +    @command.run!('/bin/cp', :args => ['-a', '--', source, target])
-     add_altname_metadata source, target
++    Hbc::Installer.new(@cask).uninstall(true)
    end
 
+   def summarize_one_link(artifact_spec)
 diff --git i/lib/hbc/artifact/symlinked.rb w/lib/hbc/artifact/symlinked.rb
-index bcaa1a3..85bb3db 100644
+index bcaa1a3..b96c7a2 100644
 --- i/lib/hbc/artifact/symlinked.rb
 +++ w/lib/hbc/artifact/symlinked.rb
-@@ -9,7 +9,7 @@ class Hbc::Artifact::Symlinked < Hbc::Artifact::Base
+@@ -4,13 +4,13 @@ class Hbc::Artifact::Symlinked < Hbc::Artifact::Base
+   end
+
+   def self.link_type_english_name
+-    'Symlink'
++    'Copy'
+   end
 
    def create_filesystem_link(source, target)
      Pathname.new(target).dirname.mkpath
 -    @command.run!('/bin/ln', :args => ['-hfs', '--', source, target])
+-    add_altname_metadata source, target
 +    @command.run!('/bin/cp', :args => ['-a', '--', source, target])
-     add_altname_metadata source, target
++    Hbc::Installer.new(@cask).uninstall(true)
    end
 
+   # Try to make the asset searchable under the target name.  Spotlight
+diff --git i/lib/hbc/installer.rb w/lib/hbc/installer.rb
+index 37a23a2..771e824 100644
+--- i/lib/hbc/installer.rb
++++ w/lib/hbc/installer.rb
+@@ -70,8 +70,6 @@ class Hbc::Installer
+       purge_versioned_files
+       raise e
+     end
+-
+-    puts summary
+   end
+
+   def summary
