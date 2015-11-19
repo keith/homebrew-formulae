45b4abc0e1a312668d6246af9e08aafe4efbfd2f
Keith Smiley <keithbsmiley@gmail.com>
2015-11-18 20:31:34 -0800

    Update ghb

--- Formula/ghb.rb
+++ Formula/ghb.rb
@@ -1,19 +1,14 @@
-require 'formula'
-
 class Ghb < Formula
   homepage 'https://github.com/keith/ghb'
   head 'https://github.com/keith/ghb.git'
+  url 'https://github.com/keith/ghb/archive/1.0.1.tar.gz'
+  sha256 '6781bc410984d1d3b3713af13ecf5e116505ed4fea81f3294135eccde3786b71'
 
   depends_on :python
 
   def install
     `pip install requests`
-    bin.install Dir['ghb*']
-    bin.install Dir['helpers']
-    zsh_completion.install '_ghb'
-  end
-
-  test do
-    system "#{ bin }/ghb"
+    bin.install Dir['src/*']
+    zsh_completion.install 'zsh/_ghb'
   end
 end
