f96c9525928c05fdaf73c9f039288d99dfb81bce
Keith Smiley <keithbsmiley@gmail.com>
2015-12-21 20:24:37 -0800

    Gifiate 0.1.1

--- Formula/gifiate.rb
+++ Formula/gifiate.rb
@@ -1,8 +1,8 @@
 class Gifiate < Formula
   homepage "https://github.com/keith/gifiate"
   head "https://github.com/keith/gifiate.git"
-  url "https://github.com/keith/gifiate/archive/0.1.0.tar.gz"
-  sha256 "41a461518ae74cfa8223eb2802fb563cf152bb6d0d8f43783e744d6684133e04"
+  url "https://github.com/keith/gifiate/archive/0.1.1.tar.gz"
+  sha256 "621eecf0f32f72ab1be0c7a23d94dc10abd4abe2779480ad8ad4287852b420ba"
 
   depends_on "ffmpeg"
   depends_on "gifsicle"
