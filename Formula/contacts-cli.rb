c6bcab531878ba794accf8562d990833883429e8
Keith Smiley <keithbsmiley@gmail.com>
2016-01-11 23:12:02 -0800

    Update contacts version

--- Formula/contacts-cli.rb
+++ Formula/contacts-cli.rb
@@ -1,8 +1,8 @@
 class ContactsCli < Formula
   homepage "https://github.com/keith/contacts-cli"
-  head "https://github.com/keith/contacts-cli.git", branch: :deployment
-  url "https://github.com/keith/contacts-cli/releases/download/1.0.0/contacts.tar.gz"
-  sha256 "d4848d2ba350e253f364ba7cf1e5a917bb668f6e5c174e5718f946498824dd75"
+  head "https://github.com/keith/contacts-cli.git"
+  url "https://github.com/keith/contacts-cli/releases/download/1.0.1/contacts.tar.gz"
+  sha256 "b9cd278b7a86e6810ae54c696d79eb159fc29bb8a91e3157ca8ccd0146664981"
 
   conflicts_with "contacts", because: "contacts also ships a `contacts` binary"
 
