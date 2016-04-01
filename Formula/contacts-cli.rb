03fd0f0b6a59da68933c2ea4b10fb1e26f33b7ed
Keith Smiley <keithbsmiley@gmail.com>
2016-04-01 13:09:05 -0700

    Update contacts-cli for Swift 2.2

--- Formula/contacts-cli.rb
+++ Formula/contacts-cli.rb
@@ -1,8 +1,8 @@
 class ContactsCli < Formula
   homepage "https://github.com/keith/contacts-cli"
   head "https://github.com/keith/contacts-cli.git"
-  url "https://github.com/keith/contacts-cli/releases/download/1.0.1/contacts.tar.gz"
-  sha256 "b9cd278b7a86e6810ae54c696d79eb159fc29bb8a91e3157ca8ccd0146664981"
+  url "https://github.com/keith/contacts-cli/releases/download/1.0.2/contacts.tar.gz"
+  sha256 "1f4dc406d78cd55985935c32c7d7ad4d9bcef6cf03656c7f61d4f76e4236ba88"
 
   conflicts_with "contacts", because: "contacts also ships a `contacts` binary"
 
