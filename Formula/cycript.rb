4dae8c2ac5fe2306d4a83b713c031de7451c38cc
Keith Smiley <keithbsmiley@gmail.com>
2016-06-02 19:58:38 -0700

    Update cycript formula

--- Formula/cycript.rb
+++ Formula/cycript.rb
@@ -1,6 +1,7 @@
 class Cycript < Formula
   homepage "http://www.cycript.org"
-  url "https://cache.saurik.com/cycript/mac/Cycript_0.9.505.zip"
+  url "https://cache.saurik.com/cycript/mac/Cycript_0.9.592.zip"
+  sha256 "05b14dd93bd0a29b8e12d527b52b3898f15d706cdf937c6d9f0897a93fe851b7"
 
   patch :DATA
 
@@ -8,17 +9,23 @@ class Cycript < Formula
     bin.install "cycript"
     lib.install Dir["Cycript.*"]
   end
+
+  def post_install
+    chmod 0755, bin/"cycript"
+  end
 end
 
 __END__
 diff --git i/cycript w/cycript
-index b20b733..f19d165 100755
+index 077458e..7874f02 100644
 --- i/cycript
 +++ w/cycript
-@@ -19,5 +19,5 @@
+@@ -17,7 +17,7 @@
  # along with this program.  If not, see <http://www.gnu.org/licenses/>.
  # }}}
 
--dir=$(dirname "${BASH_SOURCE[0]}")
-+dir=$(brew --prefix cycript)/lib
- DYLD_LIBRARY_PATH="${dir}"/Cycript.lib exec "${dir}"/Cycript.lib/cycript "$@"
+-dir=${0%/*}/Cycript.lib
++dir=($(brew --prefix cycript)/lib/Cycript.lib)
+ if [[ ${dir} != /* ]]; then
+     dir=$(pwd)/${dir}
+ fi
