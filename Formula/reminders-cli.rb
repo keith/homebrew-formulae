57116681386f2fbcb889fd93b04d2e8255b6f0e7
Aravind SV <arvind.sv@gmail.com>
2016-07-20 16:57:29 -0400

    Bump reminders-cli to 1.0.2

--- Formula/reminders-cli.rb
+++ Formula/reminders-cli.rb
@@ -1,7 +1,7 @@
 class RemindersCli < Formula
   homepage "https://github.com/keith/reminders-cli"
-  url "https://github.com/keith/reminders-cli/releases/download/1.0.1/reminders.tar.gz"
-  sha256 "909963a8d4bb9d972c32a5cd37746f93b009f4dbab2172c28932f34f5c2c05c0"
+  url "https://github.com/keith/reminders-cli/releases/download/1.0.2/reminders.tar.gz"
+  sha256 "9299512cb02c978e39a72bc94694e11d567af4ca969f5fe13bcca1ff32833d95"
 
   def install
     bin.install "reminders"
