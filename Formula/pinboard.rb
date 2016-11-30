136e901faffc58f3126a04c7903fc90107ba66cf
Keith Smiley <keithbsmiley@gmail.com>
2016-11-29 22:17:31 -0800

    Add requests dependencies

--- Formula/pinboard.rb
+++ Formula/pinboard.rb
@@ -6,8 +6,12 @@ class Pinboard < Formula
 
   depends_on :python
 
+  resource "requests" do
+    url "https://pypi.python.org/packages/6e/40/7434b2d9fe24107ada25ec90a1fc646e97f346130a2c51aa6a2b1aba28de/requests-2.12.1.tar.gz"
+    sha256 "2109ecea94df90980be040490ff1d879971b024861539abb00054062388b612e"
+  end
+
   def install
-    `pip install requests`
     bin.install Dir['src/*']
     zsh_completion.install 'zsh/_pinboard'
   end
