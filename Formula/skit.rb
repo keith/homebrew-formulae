c760c156f590adebe57765fabd7c031e2a2cbdc8
Keith Smiley <keithbsmiley@gmail.com>
2017-05-03 16:56:10 -0700

    [skit] tag release 0.1.2
    I'm not sure why I didn't create releases that you still had to build
    previously. No more `--HEAD` installs!!

--- Formula/skit.rb
+++ Formula/skit.rb
@@ -1,6 +1,8 @@
 class Skit < Formula
   homepage "https://github.com/keith/skit"
   head "https://github.com/keith/skit.git"
+  url "https://github.com/keith/skit/archive/0.1.2.tar.gz"
+  sha256 "84b7e971e6d2ae9f6e12e09d7e78bcfc9e992f909369152a2cb648ab930a015b"
 
   def install
     bin.mkpath
