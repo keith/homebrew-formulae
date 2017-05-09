173a5d31afd95402f2691979fd079cad459e12d2
Keith Smiley <keithbsmiley@gmail.com>
2017-05-08 21:57:08 -0700

    [pinboard] 1.1.1 keyboard interrupt

--- Formula/pinboard.rb
+++ Formula/pinboard.rb
@@ -1,8 +1,8 @@
 class Pinboard < Formula
   homepage 'https://github.com/keith/pinboard'
   head 'https://github.com/keith/pinboard.git'
-  url 'https://github.com/keith/pinboard/archive/1.1.0.tar.gz'
-  sha256 '0cca861d6140cfe5149442604e9666ab9516d185ad08ee9739fdbb4cdb9e404d'
+  url 'https://github.com/keith/pinboard/archive/1.1.1.tar.gz'
+  sha256 '556b0c73fc4f72760e3fbc854e00291c15f3b27da95cd7d953e281921f00cd47'
 
   depends_on :python
 
