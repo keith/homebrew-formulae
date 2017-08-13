a54bff964697d89e800e4da40a23ed65cb38eebe
Keith Smiley <keithbsmiley@gmail.com>
2017-08-13 16:07:16 -0700

    [contacts-cli] Version 1.1.1 CoreData logging fix

--- Formula/contacts-cli.rb
+++ Formula/contacts-cli.rb
@@ -1,8 +1,8 @@
 class ContactsCli < Formula
   homepage "https://github.com/keith/contacts-cli"
   head "https://github.com/keith/contacts-cli.git"
-  url "https://github.com/keith/contacts-cli/releases/download/1.1.0/contacts.tar.gz"
-  sha256 "5450a1c216f0493703551f2bc8c2a01317b06d4209f7fcf779964bb485cbb5bf"
+  url "https://github.com/keith/contacts-cli/releases/download/1.1.1/contacts.tar.gz"
+  sha256 "1dec757046ae91d1933bd87610f69d26b2d16ba91f2ce26527cfe0ad9a975e38"
 
   conflicts_with "contacts", because: "contacts also ships a `contacts` binary"
 
