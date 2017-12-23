b58c9a35336eb1b8722f747e422b77c514863929
Keith Smiley <keithbsmiley@gmail.com>
2017-12-23 14:34:06 -0800

    [tag] 0.3.0 multiple user support

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -1,8 +1,8 @@
 class Tag < Formula
   homepage "https://github.com/keith/tag"
   head "https://github.com/keith/tag.git"
-  url "https://github.com/keith/tag/releases/download/0.2.0/tag.tar.gz"
-  sha256 "feadc7d10910a74e9d3ae45a27071ab2a04bf0652a4ab0f89dd3cab185466b64"
+  url "https://github.com/keith/tag/releases/download/0.3.0/tag.tar.gz"
+  sha256 "08efa0750e0961151571feec150506586b53c309808c6f79500d5bbfb5e81a66"
 
   def install
     bin.install "tag"
