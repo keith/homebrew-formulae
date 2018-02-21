e40dce173824d0d5addea6adccf93ba4644c0ef5
Keith Smiley <keithbsmiley@gmail.com>
2018-02-21 13:27:00 -0800

    [circle-cli] Fix brew python syntax

--- Formula/circle-cli.rb
+++ Formula/circle-cli.rb
@@ -4,7 +4,7 @@ class CircleCli < Formula
   url 'https://github.com/keith/circle-cli/archive/0.1.0.tar.gz'
   sha256 'ce7405691d971ecc6cf817e7b315fb8a99c1cfb4f5a69556b703be267c752b89'
 
-  depends_on :python
+  depends_on "python"
 
   resource "requests" do
     url "https://pypi.python.org/packages/16/09/37b69de7c924d318e51ece1c4ceb679bf93be9d05973bb30c35babd596e2/requests-2.13.0.tar.gz"
