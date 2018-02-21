4b2b41f500ef97d9447fcbacbffb35e8e77a3b29
Keith Smiley <keithbsmiley@gmail.com>
2018-02-21 14:22:15 -0800

    [pinboard] Fix homebrew python dependency

--- Formula/pinboard.rb
+++ Formula/pinboard.rb
@@ -4,7 +4,7 @@ class Pinboard < Formula
   url 'https://github.com/keith/pinboard/archive/1.1.1.tar.gz'
   sha256 '556b0c73fc4f72760e3fbc854e00291c15f3b27da95cd7d953e281921f00cd47'
 
-  depends_on :python
+  depends_on "python"
 
   resource "requests" do
     url "https://pypi.python.org/packages/6e/40/7434b2d9fe24107ada25ec90a1fc646e97f346130a2c51aa6a2b1aba28de/requests-2.12.1.tar.gz"
