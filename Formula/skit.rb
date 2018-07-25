50e6e98a6d6b35cb1b63e1e8596e89fc8fb2d8e6
Keith Smiley <keithbsmiley@gmail.com>
2018-07-25 15:52:29 -0700

    [skit] Update to 0.3.0

--- Formula/skit.rb
+++ Formula/skit.rb
@@ -1,8 +1,8 @@
 class Skit < Formula
   homepage "https://github.com/keith/skit"
   head "https://github.com/keith/skit.git"
-  url "https://github.com/keith/skit/archive/0.2.0.tar.gz"
-  sha256 "a1e2d13ded95f179dafc3d4d0baf40f3779ddb61d5c07a84d3ab277874f9080c"
+  url "https://github.com/keith/skit/archive/0.3.0.tar.gz"
+  sha256 "aa017e49c0e18bb7142675d34d6abe6d94cff89849622282474d32d84ba4217b"
 
   def install
     bin.mkpath
