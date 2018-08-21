2855f01fe6a3498255d1fba608329c90d6148efa
Keith Smiley <keithbsmiley@gmail.com>
2018-08-21 00:22:41 -0700

    [git-relative-status] Make rust a build only dep
    This doesn't actually help right now since this only applies to bottles.
    Maybe one day there will be a way to make this work for head only
    builds.

--- Formula/git-relative-status.rb
+++ Formula/git-relative-status.rb
@@ -4,7 +4,7 @@ class GitRelativeStatus < Formula
   url "https://github.com/keith/git-relative-status/releases/download/0.1.0/git-relative-status.tar.gz"
   sha256 "2b75fbba327bad83b824cc80d818b816e23d89254e03a1bf92fc0991f0e50f24"
 
-  depends_on "rust"
+  depends_on "rust" => :build
 
   def install
     if build.head?
