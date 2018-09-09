ecc2256a06588752c38cb99ffc61e272e87975ca
Keith Smiley <keithbsmiley@gmail.com>
2018-09-09 15:20:26 -0700

    [git-relative-status] Update to 0.2.0
    This release fixes quote handling and handles renames.

--- Formula/git-relative-status.rb
+++ Formula/git-relative-status.rb
@@ -1,8 +1,8 @@
 class GitRelativeStatus < Formula
   homepage "https://github.com/keith/git-relative-status"
   head "https://github.com/keith/git-relative-status.git"
-  url "https://github.com/keith/git-relative-status/releases/download/0.1.0/git-relative-status.tar.gz"
-  sha256 "2b75fbba327bad83b824cc80d818b816e23d89254e03a1bf92fc0991f0e50f24"
+  url "https://github.com/keith/git-relative-status/releases/download/0.2.0/git-relative-status.tar.gz"
+  sha256 "ad7bb4f8f2bb73c77ffaf1a117482d1e92e1c522e552f81a4c12871cd7287c48"
 
   depends_on "rust" => :build
 
