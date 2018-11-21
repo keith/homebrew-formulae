63abb7490c2a88a9e4f1224913ed0cf051af386d
Keith Smiley <keithbsmiley@gmail.com>
2018-11-20 19:35:43 -0800

    [git-relative-status] Update to 0.3.0

--- Formula/git-relative-status.rb
+++ Formula/git-relative-status.rb
@@ -1,8 +1,8 @@
 class GitRelativeStatus < Formula
   homepage "https://github.com/keith/git-relative-status"
   head "https://github.com/keith/git-relative-status.git"
-  url "https://github.com/keith/git-relative-status/releases/download/0.2.0/git-relative-status.tar.gz"
-  sha256 "ad7bb4f8f2bb73c77ffaf1a117482d1e92e1c522e552f81a4c12871cd7287c48"
+  url "https://github.com/keith/git-relative-status/releases/download/0.3.0/git-relative-status.tar.gz"
+  sha256 "48ba2c3c79ad00b7e8610e3ae794deb8dc4df1ff1279e5d9794f47513b902dc8"
 
   depends_on "rust" => :build
 
