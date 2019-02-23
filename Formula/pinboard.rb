32cce1aed4bc2f0fcd68662457953f14f40c9753
Keith Smiley <keithbsmiley@gmail.com>
2019-02-23 15:36:51 -0800

    [pinboard] Update to 1.2.0

--- Formula/pinboard.rb
+++ Formula/pinboard.rb
@@ -1,18 +1,39 @@
 class Pinboard < Formula
+  include Language::Python::Virtualenv
   homepage 'https://github.com/keith/pinboard'
   head 'https://github.com/keith/pinboard.git'
-  url 'https://github.com/keith/pinboard/archive/1.1.1.tar.gz'
-  sha256 '556b0c73fc4f72760e3fbc854e00291c15f3b27da95cd7d953e281921f00cd47'
+  url 'https://github.com/keith/pinboard/archive/1.2.0.tar.gz'
+  sha256 '00aabdaa6ec917dfc8ee36ef60f556ec1827944c399d6b6f9842d83fef184750'
 
-  depends_on "python"
+  depends_on "python@2"
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/55/54/3ce77783acba5979ce16674fc98b1920d00b01d337cfaaf5db22543505ed/certifi-2018.11.29.tar.gz"
+    sha256 "47f9c83ef4c0c621eaef743f133f09fa8a74a9b75f037e8624f83bd1b6626cb7"
+  end
+
+  resource "chardet" do
+    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
+    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/65/c4/80f97e9c9628f3cac9b98bfca0402ede54e0563b56482e3e6e45c43c4935/idna-2.7.tar.gz"
+    sha256 "684a38a6f903c1d71d6d5fac066b58d7768af4de2b832e426ec79c30daa94a16"
+  end
 
   resource "requests" do
-    url "https://pypi.python.org/packages/6e/40/7434b2d9fe24107ada25ec90a1fc646e97f346130a2c51aa6a2b1aba28de/requests-2.12.1.tar.gz"
-    sha256 "2109ecea94df90980be040490ff1d879971b024861539abb00054062388b612e"
+    url "https://files.pythonhosted.org/packages/97/10/92d25b93e9c266c94b76a5548f020f3f1dd0eb40649cb1993532c0af8f4c/requests-2.20.0.tar.gz"
+    sha256 "99dcfdaaeb17caf6e526f32b6a7b780461512ab3f1d992187801694cba42770c"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/b1/53/37d82ab391393565f2f831b8eedbffd57db5a718216f82f1a8b4d381a1c1/urllib3-1.24.1.tar.gz"
+    sha256 "de9529817c93f27c8ccbfead6985011db27bd0ddfcdb2d86f3f663385c6a9c22"
   end
 
   def install
-    bin.install Dir['src/*']
+    virtualenv_install_with_resources
     zsh_completion.install 'zsh/_pinboard'
   end
 end
