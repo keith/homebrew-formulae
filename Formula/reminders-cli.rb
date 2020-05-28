6f91451f0d58f110b089b5e9559aad0086ab879a
Keith Smiley <keithbsmiley@gmail.com>
2020-05-28 09:25:26 -0700

    [reminders-cli] Update to 1.2.0

--- Formula/reminders-cli.rb
+++ Formula/reminders-cli.rb
@@ -1,7 +1,7 @@
 class RemindersCli < Formula
   homepage "https://github.com/keith/reminders-cli"
-  url "https://github.com/keith/reminders-cli/releases/download/1.1.1/reminders.tar.gz"
-  sha256 "7dddd116b79d76aedde526ec843147e840a79309cfca48960abb7d4ce349fdb2"
+  url "https://github.com/keith/reminders-cli/releases/download/1.2.0/reminders.tar.gz"
+  sha256 "3d0147ce505cae63d795e49a2f69f942464a8a704f8ad9c313111871579cd0e0"
 
   def install
     bin.install "reminders"
