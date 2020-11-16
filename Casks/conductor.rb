6c9691660c0bd3060e37c14890bcd2414cca0ba9
Keith Smiley <keithbsmiley@gmail.com>
2020-11-15 22:06:37 -0800

    [conductor] Update to 1.5.0

--- Casks/conductor.rb
+++ Casks/conductor.rb
@@ -1,6 +1,6 @@
 cask 'conductor' do
-  version '1.4.1'
-  sha256 'd0c467071f40a278b09fe3d82fa21745e86cec88246cd016b7a87bfa477a9c49'
+  version '1.5.0'
+  sha256 '0059f0026cebf1d6354ddadc67ff34ed45df312b25c0b62208c713d3ee7515b0'
 
   url "https://github.com/keith/conductor/releases/download/#{version}/Conductor.app.zip"
   appcast 'https://github.com/keith/conductor/releases.atom'
