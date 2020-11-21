ecb3069772ba85d127413f37a95167088a4aeb4e
Keith Smiley <keithbsmiley@gmail.com>
2020-11-20 23:00:47 -0800

    [modmove] Add 1.1.1
    It's easier for me to maintain this here than upstream.

---
+++ Casks/modmove.rb
@@ -0,0 +1,12 @@
+cask 'modmove' do
+  version '1.1.1'
+  sha256 '81b9cd96050b6bffecccb1ec6ef590a4fc0225c86e96de0a67a482b80c241bf7'
+
+  url "https://github.com/keith/modmove/releases/download/#{version}/ModMove.app.zip"
+  appcast 'https://github.com/keith/modmove/releases.atom'
+  name 'ModMove'
+  desc 'Easily move windows with keyboard modifiers'
+  homepage 'https://github.com/keith/modmove'
+
+  app 'ModMove.app'
+end
