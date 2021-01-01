1561e233eb72127b88ab9880ab9df7693e52dd65
Keith Smiley <keithbsmiley@gmail.com>
2021-01-01 14:18:56 -0800

    [tag] Update to 0.5.1

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -1,8 +1,8 @@
 class Tag < Formula
   homepage "https://github.com/keith/tag"
   head "https://github.com/keith/tag.git"
-  url "https://github.com/keith/tag/releases/download/0.5.0/tag.tar.gz"
-  sha256 "1180cbf890164324f8429f1e0ce842e34bc6084413d5501aebe61386e9030a17"
+  url "https://github.com/keith/tag/releases/download/0.5.1/tag.tar.gz"
+  sha256 "55e0b2dad56c9fbfc37e0631e3b9082e086c72b9b6c3b071a359b42ec36b1aad"
 
   def install
     bin.install "tag"
