39ce0b0a83c311e886f46dc3075eb2ec70f221db
Keith Smiley <keithbsmiley@gmail.com>
2021-05-30 11:43:16 -0700

    [normalize-profile] Add head URL

--- Formula/normalize-profile.rb
+++ Formula/normalize-profile.rb
@@ -2,6 +2,7 @@ class NormalizeProfile < Formula
   desc "Output provisioning profiles in a diffable way"
   homepage "https://github.com/keith/normalize-profile"
   url "https://github.com/keith/normalize-profile/archive/refs/tags/1.0.0.tar.gz"
+  head "https://github.com/keith/normalize-profile.git", branch: "main"
   sha256 "e1a6a7648de267295cc0121274768991a515c9bd9e54ee139f54c47fdf75a21b"
   license "MIT"
 
