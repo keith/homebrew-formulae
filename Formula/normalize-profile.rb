d0f82d41a57adf64092b2f7779e2faab77879f90
Keith Smiley <keithbsmiley@gmail.com>
2021-10-04 13:18:52 -0700

    [normalize-profile] Update to 1.2.0

--- Formula/normalize-profile.rb
+++ Formula/normalize-profile.rb
@@ -1,9 +1,9 @@
 class NormalizeProfile < Formula
   desc "Output provisioning profiles in a diffable way"
   homepage "https://github.com/keith/normalize-profile"
-  url "https://github.com/keith/normalize-profile/archive/refs/tags/1.1.0.tar.gz"
+  url "https://github.com/keith/normalize-profile/archive/refs/tags/1.2.0.tar.gz"
   head "https://github.com/keith/normalize-profile.git", branch: "main"
-  sha256 "1dfe847f051c50427f7066d5ef7c2a59936baa5fbb357f46e29f6a4daa6553ba"
+  sha256 "4d8dc2e425d1ea8ca837b50c752b41683004df5f9bd4540fc72d3ce33bf6c584"
   license "MIT"
 
   def install
