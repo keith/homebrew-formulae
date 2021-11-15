8b2895146360c44002f803c4902f2e3993b3cacc
Keith Smiley <keithbsmiley@gmail.com>
2021-11-14 22:14:31 -0800

    [tag] Update to 0.7.0

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -1,10 +1,15 @@
 class Tag < Formula
+  desc "Add shell aliases based on the results of searches"
   homepage "https://github.com/keith/tag"
-  head "https://github.com/keith/tag.git"
-  url "https://github.com/keith/tag/releases/download/0.6.1/tag.tar.gz"
-  sha256 "e5c97b1ebce6424450c65d34fc8878ba60e04fd00061a762e805a792c049a6ac"
+  head "https://github.com/keith/tag.git", branch: "main"
+  url "https://github.com/keith/tag/archive/refs/tags/0.7.0.tar.gz"
+  sha256 "9dc1494e49d903a77625268a23a6a2ce10bcb53d2d4648528f613ea883115358"
+
+  depends_on "cmake" => :build
 
   def install
-    bin.install "tag"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 end
