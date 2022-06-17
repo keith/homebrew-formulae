fccd7c682d6877ce01eec65464f0b53f88857626
Keith Smiley <keithbsmiley@gmail.com>
2022-06-17 10:10:02 -0700

    [git-pile] Update to 0.2.0

--- Formula/git-pile.rb
+++ Formula/git-pile.rb
@@ -1,10 +1,12 @@
 class GitPile < Formula
   desc "Stacked diff support for GitHub workflows"
   homepage "https://github.com/keith/git-pile"
-  url "https://github.com/keith/git-pile/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "945a68c945db547c0c32985208c9b2982571fe63584cd25a5ebcaff404fd480c"
+  url "https://github.com/keith/git-pile/archive/refs/tags/0.2.0.tar.gz"
+  sha256 "1e2835d7062ef27f087c7cc2dd0774fe84b94fc2485770d6ec41d44483b1f04b"
 
   head "https://github.com/keith/git-pile.git"
+
+  depends_on "gh"
 
   def install
     bin.install Dir["bin/*"]
