fd1c702cc8a21de200fea02f41bb97d50a9df96b
Keith Smiley <keithbsmiley@gmail.com>
2022-06-24 21:59:08 +0800

    [swiftpm-linux-cross] Add 0.1.1

--- Formula/swiftpm-linux-cross.rb
+++ Formula/swiftpm-linux-cross.rb
@@ -1,13 +1,14 @@
 class SwiftpmLinuxCross < Formula
   desc "Cross compile from macOS to Linux with Swift Package Manager"
   homepage "https://github.com/keith/swiftpm-linux-cross"
-  url "https://github.com/keith/swiftpm-linux-cross/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "143f14d5d805b346551e5a025f2f1e65a3a9153b71ce8b91a5b85a7f9af22877"
+  url "https://github.com/keith/swiftpm-linux-cross/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "222ec80526ad51cf4ef556a5b9145371a3ce155aad89e3bdb1100be7f4a856f2"
 
   head "https://github.com/keith/swiftpm-linux-cross.git"
 
   def install
     bin.install "setup-swiftpm-toolchain"
+    bin.install "swift-autolink-extract.swift"
   end
 
   def caveats
