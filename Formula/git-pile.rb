474141933f8da2947972aabb255293d9ee6a27a6
Keith Smiley <keithbsmiley@gmail.com>
2022-08-11 15:50:52 -0700

    [git-pile] Update to 0.4.0

--- Formula/git-pile.rb
+++ Formula/git-pile.rb
@@ -1,8 +1,8 @@
 class GitPile < Formula
   desc "Stacked diff support for GitHub workflows"
   homepage "https://github.com/keith/git-pile"
-  url "https://github.com/keith/git-pile/archive/refs/tags/0.3.0.tar.gz"
-  sha256 "ca42b0a386102f28a79658c38953553d3c2d2091c6ff5fb1321547a5f1a63e73"
+  url "https://github.com/keith/git-pile/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "417fbb477904f4ac476c0c369008a42e6e17e5365c1ef1d527c001bad2656481"
 
   head "https://github.com/keith/git-pile.git"
 
