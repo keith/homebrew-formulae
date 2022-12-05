18235069b7be592305dae54025828bb8a26b075c
Keith Smiley <keithbsmiley@gmail.com>
2022-12-05 13:14:11 -0800

    [zap] Update to 1.1.0

--- Formula/zap.rb
+++ Formula/zap.rb
@@ -1,7 +1,9 @@
 class Zap < Formula
   homepage "https://github.com/keith/zap/"
-  url "https://github.com/keith/zap/archive/1.0.2.tar.gz"
-  sha256 "03427ccf96dae6cb715c0113a471f4a814918087a4ba6c33ac623272f816a9d6"
+  head "https://github.com/keith/zap.git", branch: "main"
+  url "https://github.com/keith/zap/archive/1.1.0.tar.gz"
+
+  sha256 "3eddc687450cad7c7c0d71e5e481545f4e33461191b15f85b1f0de3eaba7ce80"
 
   def install
     bin.install "zap"
