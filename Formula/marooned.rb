a7c6cf5fc02938c4d6435751520b587a99cfea92
Keith Smiley <keithbsmiley@gmail.com>
2023-07-12 18:43:40 -0700

    Modernize marooned

--- Formula/marooned.rb
+++ Formula/marooned.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Marooned < Formula
   homepage "https://github.com/keith/marooned/"
-  url "http://keith.github.io/marooned/marooned-0.2.0.tar.gz"
-  sha1 "c923fe31cea72c07de39c208d7f9c4743ff656be"
+  url "https://keith.github.io/marooned/marooned-0.2.0.tar.gz"
+  sha256 "05c79c15e2996b169d176b07937da001d9b5111638474cda09095bba76229a7b"
 
   depends_on "xcproj" => :recommended
 
