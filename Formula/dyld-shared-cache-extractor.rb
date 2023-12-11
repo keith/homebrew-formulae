6d7d4104269f08ec4121923474c9c535b3613dc6
Keith Smiley <keithbsmiley@gmail.com>
2023-12-11 11:06:51 -0800

    [dyld-shared-cache-extractor] Add 1.0.0

--- Formula/dyld-shared-cache-extractor.rb
+++ Formula/dyld-shared-cache-extractor.rb
@@ -1,8 +1,8 @@
 class DyldSharedCacheExtractor < Formula
   desc "Extract libraries from the dyld shared cache"
   homepage "https://github.com/keith/dyld-shared-cache-extractor"
-  url "https://github.com/keith/dyld-shared-cache-extractor/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "b0cc3000cda32e6f879d44c0035822f9a002ca8b4fa281a7e9648bd511470acd"
+  url "https://github.com/keith/dyld-shared-cache-extractor/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "6cad1e319589ec3e630157fce0d98b94faa5d9f45b6c02eb147e7083d4078bde"
   head "https://github.com/keith/dyld-shared-cache-extractor.git", branch: "main"
 
   depends_on "cmake" => :build
