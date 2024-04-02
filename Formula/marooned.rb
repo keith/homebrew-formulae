d76f116aefc3823a99aec24c596819095db6ef3b
Keith Smiley <keithbsmiley@gmail.com>
2024-04-01 19:02:02 -0700

    marooned: Release version 0.3.0

--- Formula/marooned.rb
+++ Formula/marooned.rb
@@ -2,10 +2,8 @@ require "formula"
 
 class Marooned < Formula
   homepage "https://github.com/keith/marooned/"
-  url "https://keith.github.io/marooned/marooned-0.2.0.tar.gz"
-  sha256 "5adda16673119d077d91a556bf7f5e279d1a15010e7861905a59253ec34c5efa"
-
-  depends_on "xcproj" => :recommended
+  url "https://keith.github.io/marooned/marooned-0.3.0.tar.gz"
+  sha256 "471c29eaef105162e8cc7270eb9fb906f24fec3e3be15432f9fb653903724d03"
 
   def install
     prefix.install "vendor"
