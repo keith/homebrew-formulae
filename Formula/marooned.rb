a65d6e9ff6c36b9361f0d47f7048dee04ef9c064
Keith Smiley <keithbsmiley@gmail.com>
2024-10-14 15:50:00 -0700

    Fix sha

--- Formula/marooned.rb
+++ Formula/marooned.rb
@@ -3,7 +3,7 @@ require "formula"
 class Marooned < Formula
   homepage "https://github.com/keith/marooned/"
   url "https://keith.github.io/marooned/marooned-0.3.0.tar.gz"
-  sha256 "471c29eaef105162e8cc7270eb9fb906f24fec3e3be15432f9fb653903724d03"
+  sha256 "b977dbe8b4d1e9931d37c5860a46173a46d064307fc58954fc63d362a86a317e"
 
   def install
     prefix.install "vendor"
