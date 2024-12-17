69c2337330a706658ab74db9805307f65b401f01
Keith Smiley <keithbsmiley@gmail.com>
2024-12-16 16:16:14 -0800

    Update tag

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -2,8 +2,8 @@ class Tag < Formula
   desc "Add shell aliases based on the results of searches"
   homepage "https://github.com/keith/tag"
   head "https://github.com/keith/tag.git", branch: "main"
-  url "https://github.com/keith/tag/archive/refs/tags/0.9.1.tar.gz"
-  sha256 "b99d910afce91694a643b581cea6cb1a6836b6a958006e80af0c37007111c8bb"
+  url "https://github.com/keith/tag/archive/refs/tags/0.10.0.tar.gz"
+  sha256 "bcd45803150a001d971ec034175e0f359ecb845c5100cc3125af323d7c77f112"
 
   depends_on "cmake" => :build
 
