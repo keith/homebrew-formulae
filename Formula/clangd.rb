8264802cc13eae5c289fb85ffbb8d793040b2f4f
Keith Smiley <keithbsmiley@gmail.com>
2025-08-06 14:30:57 -0700

    Update clangd

--- Formula/clangd.rb
+++ Formula/clangd.rb
@@ -1,7 +1,7 @@
 class Clangd < Formula
   homepage "https://clangd.llvm.org"
-  url "https://github.com/clangd/clangd/releases/download/14.0.3/clangd-mac-14.0.3.zip"
-  sha256 "9ad803ab626144e243b921dc2f2f9ecf0cd6c80ded5617c71c17cb79f4bb3887"
+  url "https://github.com/clangd/clangd/releases/download/20.1.8/clangd-mac-20.1.8.zip"
+  sha256 "c2303d0a83dcb31c08c4920e815586ad1b0c17ee8d1a484d605f33d784a31402"
 
   def install
     bin.install "bin/clangd"
